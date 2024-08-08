aabb3374cf572b2f2d3ecbd52b8d772d71ba05e7
felipe <feliperodriguesqueiroz122@gmail.com>
2024-08-07 21:14:38 -0300

    feat: Implementando funcionalidade no button, na qual lê os dados do user e transforma-os em JSON

---
+++ atividade1/script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('infoForm');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const idade = document.getElementById('idade').value;
+    const cpf = document.getElementById('cpf').value;
+
+    const formData = {
+        nome: nome,
+        idade: idade,
+        cpf: cpf
+    };
+
+    const jsonData = JSON.stringify(formData);
+
+    console.log(jsonData);
+
+    alert("Usuário cadastrado com sucesso!");
+});
