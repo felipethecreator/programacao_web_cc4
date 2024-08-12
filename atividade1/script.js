90c9d96b2882d10a8ecec4a633c80a85ee260ed2
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-08-12 10:54:02 -0300

    to nem ai pra nome nao, quero mijar, to na pressa

--- atividade1/script.js
+++ atividade1/script.js
@@ -1,11 +1,19 @@
 const form = document.getElementById('infoForm');
 
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
+    
     const nome = document.getElementById('nome').value;
     const idade = document.getElementById('idade').value;
     const cpf = document.getElementById('cpf').value;
+    const email = document.getElementById('email').value;
+    
+    function ValidarEmail () { //TODO
+        
+    }
+
+    ValidarEmail();
 
     const formData = {
         nome: nome,
@@ -16,6 +24,6 @@ form.addEventListener('submit', (event) => {
     const jsonData = JSON.stringify(formData);
 
     console.log(jsonData);
-
+    submit
     alert(`Usuário cadastrado com sucesso. Olá, ${nome}!`);
 });
