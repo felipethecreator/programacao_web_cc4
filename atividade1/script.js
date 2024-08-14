dd95dab456804c4b788973fa50a66c295aef3965
Felipe Rodrigues <feliperodriguesqueiroz122@gmail.com>
2024-08-14 09:56:42 -0300

    feat: Acrescentando função que efetua uma verificação se o e-mail é válido ou não.

--- atividade1/script.js
+++ atividade1/script.js
@@ -9,21 +9,33 @@ form.addEventListener('submit', (event) => {
     const cpf = document.getElementById('cpf').value;
     const email = document.getElementById('email').value;
     
-    function ValidarEmail () { //TODO
-        
+    function ConferirEmail() {
+        const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+        return emailRegex.test(email);
     }
 
-    ValidarEmail();
+    const ValidarEmail = (email) => {
+        if (!ConferirEmail(email)) {
+            alert("Insira um e-mail válido!")
+            return false;
+        }
+        return true;
+    }
+
+    if (!ValidarEmail(email)) {
+        return;
+    }
 
     const formData = {
         nome: nome,
         idade: idade,
-        cpf: cpf
-    };
+        cpf: cpf,
+        email: email
+       };
 
     const jsonData = JSON.stringify(formData);
 
     console.log(jsonData);
-    submit
+    
     alert(`Usuário cadastrado com sucesso. Olá, ${nome}!`);
 });
